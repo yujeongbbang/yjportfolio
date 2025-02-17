cd749524d401e68efb1eccf0f2ee4fd6f2a33102
yujeongbbang <pzzzz12@naver.com>
2025-02-17 14:53:04 +0900

    [refactor] 내용 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,15 @@ const Home = () => (
   <Element name="home" className="home-section">
     <div className="home-content">
       <div className="home-main-content">
-        <h1 className="home-main-title">사용자 중심의 개발자, 서유정입니다</h1>
-        <p className="home-sub-title">Exploring the World of Front-end Development</p>
-        <p className="home-description">
-          안녕하세요! 신입 개발자로서 창의적인 도전과 학습을 통해 웹의 미래를 만들어가고 있는 서유정입니다.
-        </p>
-        <p className="home-description"> 제 포트폴리오에서 제가 가진 다양한 기술 역량과 앞으로의 성장 가능성을 확인해 보세요.</p>
+      <h1 className="home-main-title">배우고, 만들고, 성장하는 개발자 서유정입니다.</h1>
+      <p className="home-sub-title">Keep Exploring, Keep Building, Keep Growing</p>
+      <p className="home-description">
+        새로운 기술을 배우는 것에서 멈추지 않고, 직접 만들어보고 부딪히며 성장합니다.  
+        작은 시도들이 모여 더 나은 경험과 서비스를 만든다고 믿습니다.
+      </p>
+      <p className="home-description">
+        저의 프로젝트와 고민의 과정들을 포트폴리오에서 만나보세요.
+      </p>
         <div className="home-cta">
           <Link to="introduce" smooth={true} duration={1000} className="home-cta-button">
             View My Portfolio
